074919076b21ed4d99ce6d6661867830230f3d62
prz23 <panruizheng@zju.edu.cn>
2021-07-20 15:13:21 +0800

    support sgx

--- src/lib.rs
+++ src/lib.rs
@@ -48,6 +48,8 @@ of acceptable widths to lower or upper bounds. These traits are equivalent to
 #[macro_use]
 extern crate sgx_tstd as std;
 
+use std::prelude::v1::*;
+
 use core::{
 	convert::{
 		TryFrom,
